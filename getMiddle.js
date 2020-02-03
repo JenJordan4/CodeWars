9d1338ef45ee08e48dfa571537c1e10d5a57db79
jenjordan4 <jbaldwin4@hotmail.com>
2020-02-03 16:09:08 -0500

    get middle challenge

---
+++ getMiddle.js
@@ -0,0 +1,17 @@
+//You are going to be given a word. Your job is to return the middle character of the word. 
+//If the word's length is odd, return the middle character. 
+//If the word's length is even, return the middle 2 characters.
+
+//#Example:
+//getMiddle("test") should return "es"
+
+function getMiddle(s)
+{
+  if (s.length % 2 === 1) {
+    return s.charAt(s.length/2)
+  } else {
+    return s.charAt((s.length/2)-1) + s.charAt(s.length/2)
+  }
+}
+
+getMiddle("test")
